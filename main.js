072204257676a37ee50c9230f0fdcaeb5255cac6
PHP-makaka <71174581+PHP-makaka@users.noreply.github.com>
2021-10-20 15:18:53 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 let btn =document.getElementById("btn")
 let textArera = document.getElementById("test")
+let textOtvet = document.getElementById("otvet")
+
 btn.addEventListener("click",fun)
-const arr =['affectionate',"ambitious","anxious",'bossy','charming','competitive','honest','imaginative','independent','insecure','mature','moody,\n' +
-'patient','rebellious','reliable','self-confident','selfish','sensible','sensible','socoable','spoilt','stubborn']
+const arr =[['affectionate','ласковый'],["ambitious",'амбициозный'],["anxious",'тревожный'],['bossy','властная'],['charming','очаровательный'],['competitive','конкурентноспособный'],['honest','честный'],['imaginative','творческий'],['independent','независимый'],['insecure','ненадёжный'],['mature','взрослый'],['moody,'],
+['patient','терпеливый'],['rebellious','упрямый'],['reliable','надежный'],['self-confident','самоуверенный'],['selfish','эгоистичный'],['sensible','здравомыслящий'],['senstive','чувствительный'],['socoable','общительный'],['spoilt','испорченный'],['stubborn','упрямый']]
 
 function fun(){
     let num =Math.floor(Math.random() * arr.length)
-
-    textArera.innerText=arr[num]
+    textOtvet.innerText=arr[num[1]];
+    textArera.innerText=arr[num[0]];
 }

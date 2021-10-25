fc15d7c8227bbf189a27d0db9035659ae12bead3
PHP-makaka <71174581+PHP-makaka@users.noreply.github.com>
2021-10-25 09:30:40 +0300

    Update main.js

--- main.js
+++ main.js
@@ -3,8 +3,12 @@ let textArera = document.getElementById("test")
 let textOtvet = document.getElementById("otvet")
 
 btn.addEventListener("click",fun)
-const arr =[['affectionate','ласковый'],["ambitious",'амбициозный'],["anxious",'тревожный'],['bossy','властная'],['charming','очаровательный'],['competitive','конкурентноспособный'],['honest','честный'],['imaginative','творческий'],['independent','независимый'],['insecure','ненадёжный'],['mature','взрослый'],['moody,'],
-['patient','терпеливый'],['rebellious','упрямый'],['reliable','надежный'],['self-confident','самоуверенный'],['selfish','эгоистичный'],['sensible','здравомыслящий'],['senstive','чувствительный'],['socoable','общительный'],['spoilt','испорченный'],['stubborn','упрямый']]
+const arr =[
+['affectionate','ласковый'],["ambitious",'амбициозный'],["anxious",'тревожный'],['bossy','властная'],['charming','очаровательный'],['competitive','конкурентноспособный'],['honest','честный'],['imaginative','творческий'],['independent','независимый'],['insecure','ненадёжный'],['mature','взрослый'],['moody','угрюмый'],
+['patient','терпеливый'],['rebellious','упрямый'],['reliable','надежный'],['self-confident','самоуверенный'],['selfish','эгоистичный'],['sensible','здравомыслящий'],['senstive','чувствительный'],['socoable','общительный'],['spoilt','испорченный'],['stubborn','упрямый'],['miniscule','крошечный'],['ginormous','огромноый'],['tiny','крошечный'],
+['jubilant','ликующий'],['felicitous','счастливый'],['certainly','несомненно '],['fictional','вымышленный'],['prevent','предотвращать'],['particular','конкретный'],['disease','болезнь'],
+['насильственный','насильственный,жестокий']
+]
 
 function fun(){
     let num =Math.floor(Math.random() * arr.length)
